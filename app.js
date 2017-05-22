bdda400cd2d785699589bd7ff268c81d61e436ea
Bharat Poptwani <b4bharat.available@gmail.com>
2017-05-22 20:04:03 +0530

    basic markup added for autocomplete implementation

---
+++ app.js
@@ -0,0 +1,7 @@
+/**
+ * 2. create an array of hardcoded values to search
+ * 3. on keypress, call a function that:
+ *    3.1 searches through the text entered in the input box and creates an array of search results
+ *    3.2 attaches the result to the DOM
+ * 4. DOM for listing the results
+ */
