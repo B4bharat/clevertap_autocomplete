43f06982beb97e414f36f9a8a263baed8f6875cb
Bharat Poptwani <b4bharat.available@gmail.com>
2017-05-22 20:38:56 +0530

    added the algorithm to return search results

--- app.js
+++ app.js
@@ -1,7 +1,39 @@
 /**
- * 2. create an array of hardcoded values to search
  * 3. on keypress, call a function that:
  *    3.1 searches through the text entered in the input box and creates an array of search results
  *    3.2 attaches the result to the DOM
  * 4. DOM for listing the results
  */
+let ufcFighters = [
+  'Conor McGregor',
+  'Khabib Nurmagomedov',
+  'Tony Ferguson',
+  'Eddie Alvarez',
+  'Nate Diaz',
+  'Nick Diaz',
+  'Michael Bisping',
+  'Goerges St-Pierre',
+  'Yoel Romero',
+  'Robert Whittakar',
+  'Gegard Mussasi',
+  'Luke Rockhold',
+  'Chris Weidman',
+  'Jacare Souza',
+  'Kelvin Gastelum',
+  'Anderson Silva',
+  'Daniel Cormier',
+  'Jon Jones',
+  'Alexender Gustaffson',
+  'Jimmi Manuwa',
+  'Stipe Miocic',
+  'Junior Dos Santos',
+  'Fabricio Werdum',
+  'Cain Valazquez',
+  'Mark Hunt'
+];
+
+function getSearchResults(searchTerm) {
+  return ufcFighters.filter(function(fighter) {
+    return fighter.match(new RegExp(searchTerm, 'i'));
+  });
+}
