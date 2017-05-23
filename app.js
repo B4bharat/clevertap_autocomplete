c5a64af307f9750a04d3672e5c6df8ed5634be71
Bharat Poptwani <b4bharat.available@gmail.com>
2017-05-23 21:10:11 +0530

    added array of object for demonstration

--- app.js
+++ app.js
@@ -28,6 +28,36 @@ $(document).ready(function() {
     'Mark Hunt'
   ];
 
+  /**
+   * optionally you can add this array of objects and supply a 'lookupProperty' property with value-'name' to autocomplete method
+   * let ufcFighters = [
+      {
+        name: 'Conor McGregor',
+        age: 28
+      },
+      {
+        name: 'Khabib Nurmagomedov',
+        age: 28
+      },
+      {
+        name: 'Tony Ferguson',
+        age: 28
+      },
+      {
+        name: 'Eddie Alvarez',
+        age: 28
+      },
+      {
+        name: 'Nate Diaz',
+        age: 28
+      },
+      {
+        name: 'Nick Diaz',
+        age: 28
+      }
+    ];
+   */
+
   $('#search-box').autocomplete({
     lookup: ufcFighters
   });
