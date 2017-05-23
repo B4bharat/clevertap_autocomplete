d940f5fe14bc4b6724b902ff6a5f20bdd42f030e
Bharat Poptwani <b4bharat.available@gmail.com>
2017-05-23 19:03:59 +0530

    minor code reindentation

--- app.js
+++ app.js
@@ -123,10 +123,10 @@ Plugin.prototype.init = function () {
 // preventing against multiple instantiations
 $.fn[pluginName] = function ( options ) {
   return this.each(function () {
-      if (!$.data(this, 'plugin_' + pluginName)) {
-          $.data(this, 'plugin_' + pluginName, 
-          new Plugin( this, options ));
-      }
+    if (!$.data(this, 'plugin_' + pluginName)) {
+      $.data(this, 'plugin_' + pluginName, 
+      new Plugin( this, options ));
+    }
   });
 }
 }(jQuery, window, document));
